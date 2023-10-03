d6a143cd190290473575d184a61912862b244523
“AngieNC” <139162495+AngieNC@users.noreply.github.com>
2023-10-02 23:22:31 -0500

    docs: :beers: Trato de actualizar ahí mismo
    Se intenta actualizar ahi mismo cuando el usuario modifica la base de datos

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-const url = "https://6509e7ebf6553137159c3aee.mockapi.io/Pokemon";
+//Url mockapi
+const url = "http://127.0.0.1:5010/pokemon";
 
+//Traigo pokeapi 
 const vengaPokemon = async () => {
-  const res = await (await fetch("https://pokeapi.co/api/v2/pokemon?limit=500")).json();
+  const res = await (await fetch("http://127.0.0.1:5010/pokemon?limit=500")).json();
   return res.results.map(pokemon => pokemon.name);
 }
 
@@ -10,68 +12,52 @@ const pokemon = async () => {
   const principal = document.querySelector("section");
 
   for (const name of nombres) {
-    const res = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)).json();
+
+    const res = await (await fetch(`http://127.0.0.1:5010/pokemon/${name}`)).json();
+
     const div = document.createElement("div");
     div.innerHTML = `
       <img src="${res.sprites.front_default}"/>  
       <button class="botones" id="principal" src="${res.sprites.front_default}"><h2 id="h2">${name}</h2></button>
       <img src="${res.sprites.front_default}"/>
     `;
+
     principal.appendChild(div);
 
     const muestreBoton = div.querySelector(".botones");
     muestreBoton.addEventListener("click", async () => {
+      
       const imageUrl = res.sprites.front_default;
+      
       const nombrePokemon = res.name;
 
       // Comprobar si el Pokémon ya existe en la API mock
-      const existeEnMockApi = await verificar (nombrePokemon);
+      const existeEnMockApi = await verificarExistenciaPokemonEnMockApi(nombrePokemon);
 
       Swal.fire({
         title: `${nombrePokemon}`,
         imageUrl: imageUrl,
         imageAlt: "Imagen del Pokémon",
         html: `
-            <form id="formulario-${nombrePokemon}">
-                ${res.stats.map((data) =>`
-                <div>
-                    <input type="range" value="${data.base_stat}" name="${data.stat.name}">
-                    <label data-name="${data.stat.name}"><b>${data.base_stat}</b> <b>${data.stat.name}</b></label><br>
-                </div>
-                `).join("")}
-                <input type="submit" class="segundo_boton" value="Actualizar"/> 
-            </form>
+          <form id="formulario-${nombrePokemon}">
+            ${res.stats.map((data) =>`
+              <div>
+                <input type="range" value="${data.base_stat}" name="${data.stat.name}">
+                <label data-name="${data.stat.name}"><b>${data.base_stat}</b> <b>${data.stat.name}</b></label><br>
+              </div>
+            `).join("")}
+            <input type="submit" class="segundo_boton" value="${existeEnMockApi ? 'Actualizar' : 'Guardar'}"/> 
+          </form>
         `,
         imageWidth: "80%",
         imageHeight: "80%"
-      });
+      });        //Si existe llama la funcion actualizar o sino llama la funcion guardar
 
       const formulario = document.querySelector(`#formulario-${nombrePokemon}`);
       formulario.addEventListener("submit", async (e) => {
         e.preventDefault();
-        //const enviar = Object.fromEntries(new FormData(e.target));
-        let contenedor = document.querySelector('#swal2-html-container');
-        contenedor.addEventListener("input", (e) => {
-          let hermano = e.target.nextElementSibling;
-          hermano.innerHTML = `<b>${e.target.value}</b> ${hermano.dataset.name}`;
-        });
-
-        const actualizar = async(name, id, imageUrl, stats)=>{
-          try{
-            const responde = await fetch(`${url}/${id}`,{
-              method: "PUT",
-              headers: {"Content-Type":"application/json"},
-              body: JSON.stringify({ name: name, imageUrl: imageUrl, stats: stats}),
-            });
-            const data = await responde.json();
-            console.log(`Pokemon ${data.name} check`)
+        const enviar = Object.fromEntries(new FormData(e.target)); //Crear un nuevo json con la data 
         
-            }catch(error){
-              console.error("Omg", error)
-          }
-        }
-        actualizar()
-        /*
         try {
           if (existeEnMockApi) {
             // Si el Pokémon existe en la API mock, actualizar
@@ -83,9 +69,9 @@ const pokemon = async () => {
             const response = await fetch(`${url}/${nombrePokemon}`, config);
 
             if (response.ok) {
-              console.log("Datos actualizados con éxito en la API mock.");
+              console.log("Datos actualizados");
             } else {
-              console.error("Error al actualizar datos en la API mock.");
+              console.error("Error al actualizar datos en la API");
             }
           } else {
             // Si el Pokémon no existe en la API mock, crear uno nuevo
@@ -97,35 +83,36 @@ const pokemon = async () => {
             const response = await fetch(url, config);
 
             if (response.ok) {
-              console.log("Nuevo Pokémon creado con éxito en la API mock.");
+              console.log("Nuevo Pokémon creado con éxito");
             } else {
-              console.error("Error al crear un nuevo Pokémon en la API mock.");
+              console.error("Error al crear un nuevo Pokémon en la API");
             }
           }
         } catch (error) {
           console.error("Error al interactuar con la API mock:", error);
-        }*/
+        }
       });
-      
+
+      let contenedor = document.querySelector('#swal2-html-container');
+      contenedor.addEventListener("input", (e) => {
+        let hermano = e.target.nextElementSibling;
+        hermano.innerHTML = `<b>${e.target.value}</b> ${hermano.dataset.name}`;
       });
-      
-    }
-  
+    });
+  }
 }
 
-
-pokemon();
-
 // Función para verificar si un Pokémon existe en la API mock
-/*
-const verificar = async (nombrePokemon) => {
+const verificarExistenciaPokemonEnMockApi = async (nombrePokemon) => {
   try {
     const response = await fetch(`${url}?name=${nombrePokemon}`);
     const data = await response.json();
-    return data.length > 0;
+    return data.length > 0; //Retorna el tamaño del json
   } catch (error) {
-    console.error("Error al verificar existencia del Pokémon en la API mock:", error);
+    console.error("Error al verificar existencia del Pokémon en la API", error);
     return false;
   }
 }
-*/
+
+pokemon();
+
